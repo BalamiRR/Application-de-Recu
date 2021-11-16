519388c1536f6f6ba16472304d3253b20b562745
Fuat Kara -                                 Junior Java Developer <36539306+BalamiRR@users.noreply.github.com>
2021-11-16 21:04:40 +0100

    java_project

--- src/MyPackage/InsertTicket.java
+++ src/MyPackage/InsertTicket.java
@@ -93,7 +93,6 @@ public class InsertTicket {
 				String name = rs.getString("NomDepartement");
 				combo_depart.addItem(name);
 				}
-			
 		}catch (Exception b) {
 			JOptionPane.showMessageDialog(null, b);
 		}
